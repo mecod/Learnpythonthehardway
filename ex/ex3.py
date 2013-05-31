8d00d7726923db630567677a930c6e837bc7677f
mecod <mecod@msn.com>
2013-05-31 23:35:47 +0800

    增加练习1 增加练习2 增加练习3

---
+++ ex/ex3.py
@@ -0,0 +1,27 @@
+# 打印小鸡标题
+print "I will now count my chickens:"
+# 打印数字
+print "Hens", 25 + 30 / 6
+print "Roosters", 100.00 - 25 * 3 % 4
+# 打印鸡蛋标题
+print "Now I wil count the eggs:"
+# 打印鸡蛋个数
+print 3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4 + 6
+# 打印判断标题
+print "Is it true that 3 + 2 < 5 - 7?"
+# 打印判断值：True或False
+print 3 + 2 < 5 - 7
+# 打印加法
+print "What is 3 + 2?", 3 + 2
+# 打印减法
+print "What is 5 - 7?", 5 - 7
+# 打印感叹句
+print "Oh, that's why it's False."
+# 打印承接句
+print "How about some more."
+# 打印True，使用大于号
+print "Is it greater?", 5 > -2
+# 打印True，使用大于等于号
+print "Is it greater or equal?", 5 >= -2
+# 打印False，使用小于等于号
+print "Is it less or equal?", 5 <= -2
